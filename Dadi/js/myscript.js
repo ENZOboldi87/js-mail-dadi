27466cf75fb4b6381ca7a2a0a8da07cd9eb28317
Giuseppe Rotolo <rotolo37@gmail.com>
2020-05-25 14:45:14 +0200

    sistemazione cartelle

--- Dadi/js/myscript.js
+++ Dadi/js/myscript.js
@@ -1,24 +1,4 @@
-var richiestaEmail = prompt('Inserici la tua Email');
-var emailUtenti = [ "pinco@gmail.com", "pallino@gmail.com", "gigino@gmail.com", "boldino@gmail.com", "vertino@gmail.com", "giullino@gmail.com",];
-var convalida = false;
 
-
-for (var i = 0; i < emailUtenti.length; i++) {
-
-  var emailValida = emailUtenti[i];
-
-  if (richiestaEmail == emailValida) {
-    convalida = true;
-    console.log('hai diritto ad acccedere');
-
-  }
-
-  else if (richiestaEmail !== emailValida) {
-    convalida = false;
-    console.log('non hai diritto ad acccedere');
-
-  }
-}
 
 
 // var computerNumeriDadi = Math.floor(Math.random() * 6) + 1;
