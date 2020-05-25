6a7936bdbe78fd98c07abcb4ad518959894fcdd9
Giuseppe Rotolo <rotolo37@gmail.com>
2020-05-25 18:14:18 +0200

    dadi e mail ready

--- Dadi/js/myscript.js
+++ Dadi/js/myscript.js
@@ -1,11 +1,27 @@
+// variabili
+var numeriUtenti = parseInt(prompt('inserisci un numero da 1 a 6'));
+var computerNumDadi = Math.floor(Math.random() * 6) + 1;
+var vittoria = false;
 
+// risultati
+  if (numeriUtenti > computerNumDadi) {
+    // in caso di vittoria
+    vittoria = true;
+    console.log('il computer ha tirato ' + computerNumDadi );
+    console.log('tu hai tirato ' + numeriUtenti );
+    console.log('hai vinto');
 
-
-// var computerNumeriDadi = Math.floor(Math.random() * 6) + 1;
-// console.log(computerNumeriDadi);
-//
-// var utenteNumeriDadi = parseInt(prompt('Inserisci un numero da 1 a 6'));
-//
-// for (var i = 0; i < computerNumeriDadi.lenght; i++) {
-//   console.log(computerNumeriDadi);
-// }
+  }
+  else if (numeriUtenti < computerNumDadi) {
+    // in caso di sconfitta
+    vittoria = false;
+    console.log('il computer ha tirato ' + computerNumDadi );
+    console.log('tu hai tirato ' + numeriUtenti );
+    console.log('hai perso');
+  }
+  else {
+    // in caso di pareggio
+    console.log('il computer ha tirato ' + computerNumDadi );
+    console.log('tu hai tirato '  + numeriUtenti );
+    console.log('pareggio');
+  }
--- Mail/js/myscript.js
+++ Mail/js/myscript.js
@@ -1,6 +1,9 @@
+// variabili
 var richiestaEmail = prompt('Inserici la tua Email');
 var emailUtenti = [ "pinco@gmail.com", "pallino@gmail.com", "gigino@gmail.com", "boldino@gmail.com", "vertino@gmail.com", "giullino@gmail.com",];
 var convalida = false;
+var risMessaggio = document.getElementById('Risultato');
+var messaggio;
 
 
 for (var i = 0; i < emailUtenti.length; i++) {
@@ -8,24 +11,18 @@ for (var i = 0; i < emailUtenti.length; i++) {
   var emailValida = emailUtenti[i];
 
   if (richiestaEmail == emailValida) {
+    // email presente
     convalida = true;
-    console.log('hai diritto ad acccedere');
-
+    messaggio = ('hai diritto ad acccedere');
   }
 
   else if (richiestaEmail !== emailValida) {
+    // email mancante
     convalida = false;
-    console.log('non hai diritto ad acccedere');
+    messaggio = ('non hai diritto ad acccedere');
 
   }
+
+    risMessaggio.innerHTML = messaggio;
+
 }
-
-
-// var computerNumeriDadi = Math.floor(Math.random() * 6) + 1;
-// console.log(computerNumeriDadi);
-//
-// var utenteNumeriDadi = parseInt(prompt('Inserisci un numero da 1 a 6'));
-//
-// for (var i = 0; i < computerNumeriDadi.lenght; i++) {
-//   console.log(computerNumeriDadi);
-// }
