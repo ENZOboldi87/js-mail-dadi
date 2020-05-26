a08264e61bb018d5ed72a89534503c20fb2d84ac
Giuseppe Rotolo <rotolo37@gmail.com>
2020-05-26 10:52:12 +0200

    js validate

--- Mail/js/myscript.js
+++ Mail/js/myscript.js
@@ -1,6 +1,6 @@
 // variabili
-var richiestaEmail = prompt('Inserici la tua Email');
-var emailUtenti = [ "pinco@gmail.com", "pallino@gmail.com", "gigino@gmail.com", "boldino@gmail.com", "vertino@gmail.com", "giullino@gmail.com",];
+var richiestaEmail = prompt("Inserici la tua Email");
+var emailUtenti = ["pinco@gmail.com", "pallino@gmail.com", "gigino@gmail.com", "boldino@gmail.com", "vertino@gmail.com", "giullino@gmail.com"];
 var convalida = false;
 
 
@@ -10,13 +10,13 @@ for (var i = 0; i < emailUtenti.length; i++) {
 // email convalidata
   if (richiestaEmail === emailUtenti[i]) {
     convalida = true;
-    document.getElementById('Risultato').innerHTML = ('sei autorizzato')
+    document.getElementById("Risultato").innerHTML = ("sei autorizzato")
 
   }
 
 }
 // se email non convalidata
 if (convalida == false) {
-  document.getElementById('Risultato').innerHTML = (' non sei autorizzato')
+  document.getElementById("Risultato").innerHTML = (" non sei autorizzato")
 
 }
